794d489c9abbf9eb67d059b3de4c7606a6e6a051
SampietroHugo <victorhsampietro2@gmail.com>
2025-10-22 23:14:35 -0300

    ícones de troca e shiny na galeria

--- js/inserirPokeGaleria.js
+++ js/inserirPokeGaleria.js
@@ -11,17 +11,33 @@ fetch('/json/pokedex-galeria.json')
 
         pokemonsOrdenados.forEach(p => {
             const div = document.createElement('div');
-            div.className = `pokemon ${p.type} ${p.shiny ? 'shiny' : ''}`;
+            
+            div.className = `pokemon ${p.type} ${p.shiny ? 'shiny' : ''} ${p.trade ? 'trade' : ''}`;
+            
             div.innerHTML = `
-        <div class="imgContainer">
-          <img src="${p.image}" alt="${p.alt}">
-        </div>
-        <div class="info ${p.shiny ? 'shiny' : ''}">
-          <p><strong>${p.name}</strong></p>
-          <p><strong>${p.number}</strong></p>
-          <p><strong>${p.game}</strong></p>
-          <p><strong>${p.date}</strong></p>
-        </div>`;
+            <div class="imgContainer">
+              
+              <div class="icon-container">
+                ${p.shiny ? '<span class="icon shiny-icon" title="Shiny"></span>' : ''}
+                ${p.trade ? '<span class="icon trade-icon" title="Trocado"></span>' : ''}
+              </div>
+              
+              <img src="${p.image}" alt="${p.alt}">
+            </div>
+            <div class="info">
+              <div class="name-wrapper">
+                <p><strong>${p.name}</strong></p>
+              </div>
+              <p><strong>${p.number}</strong></p>
+              <p><strong>${p.game}</strong></p>
+              <p><strong>${p.date}</strong></p>
+            </div>`;
+            
             container.appendChild(div);
         });
+    })
+    .catch(error => {
+        console.error('Erro ao carregar o JSON dos Pokémons:', error);
+        const container = document.querySelector('.pokeContainer');
+        container.innerHTML = "<p style='color: white; font-family: sans-serif;'>Erro ao carregar a Pokédex. Verifique o console.</p>";
     });
